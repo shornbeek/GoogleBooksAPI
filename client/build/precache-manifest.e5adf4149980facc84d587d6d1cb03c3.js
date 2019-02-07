b7e3915ec9d47b4ff120a0adab724bf217913665
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-07 00:01:05 -0800

    dir switch

---
+++ client/build/precache-manifest.e5adf4149980facc84d587d6d1cb03c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7435f933aa2a22ea21cb",
+    "url": "/static/css/main.eacfc815.chunk.css"
+  },
+  {
+    "revision": "7435f933aa2a22ea21cb",
+    "url": "/static/js/main.7435f933.chunk.js"
+  },
+  {
+    "revision": "ba250e2b2fa48dea3682",
+    "url": "/static/js/1.ba250e2b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f3857b6097ff666a0615ef6ad5a7902",
+    "url": "/static/media/books-2.4f3857b6.jpg"
+  },
+  {
+    "revision": "4a6a171a1415e1c3fa05cde5ef47008b",
+    "url": "/index.html"
+  }
+];
